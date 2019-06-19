c083fb13b4dcf58d8850a02d2cf250f92a4f07a3
AlihanAt <alihan_a98@hotmail.de>
2019-06-19 17:13:10 +0200

    Waende

---
+++ Assets/Scripts/SpawnUnbreakable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnUnbreakable : MonoBehaviour
+{
+    private GridScript grid;
+    public GameObject unbreakableWall;
+    private float posX = 1.5f;
+    private float posZ = 1.5f;
+    private readonly float posY = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = FindObjectOfType<GridScript>();
+        PlaceWalls();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void PlaceWalls()
+    {
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Instantiate(unbreakableWall, new Vector3(posX, posY, posZ), Quaternion.identity);
+                    posZ += 2;
+                }
+                posZ = 1.5f;
+                posX += 2;
+            }
+        
+    }
+}
