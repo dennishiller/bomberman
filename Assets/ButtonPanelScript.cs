d74101d992af98b1d24d9cf9d268dcd7c4aa15f4
Sero7777 <max_wa@gmx.de>
2019-06-21 13:16:37 +0200

    menu

---
+++ Assets/ButtonPanelScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonPanelScript : MonoBehaviour
+{
+    public void loadGame()
+    {
+        // Spielt nächste sequenz ab
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
+
+
