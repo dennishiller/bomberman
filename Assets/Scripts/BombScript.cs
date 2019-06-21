e369632210a6d789038ad031db64756910f3e3d2
Sero7777 <max_wa@gmx.de>
2019-06-21 14:33:31 +0200

    merged menu with gaming scene

---
+++ Assets/Scripts/BombScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour
+{
+    private float lifeTime = 3;
+    //public GameObject player;
+    private MovementPlayer mp;
+    new Collider collider;
+ 
+    //IEnumerator countdown()
+    //{
+    //    yield return new WaitForSecondsRealtime(lifeTime);
+    //    isActive = false;
+    //}
+
+    void Awake()
+    {
+        //StartCoroutine(countdown());
+        Destroy(gameObject, lifeTime);
+        collider = GetComponent<Collider>();
+        collider.enabled = false;
+        mp = FindObjectOfType<MovementPlayer>();
+
+    }
+
+    private void OnDestroy()
+    {
+        mp.BombExploded();
+        //player.GetComponent<MovementPlayer>().BombExploded();
+        //Debug.Log("Destroy");
+    }
+
+
+}
---
+++ Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    Rigidbody rb;
+    //public float speed = 3f;
+    public GameObject bomb;
+    private int maxBombs = 2;
+    private int bombsOnField=0;
+    private List<GameObject> bombList = new List<GameObject>();
+    private float cooldownTime=1;
+    private float nextBombTime=0;
+    private new Collider collider;
+    private GridScript grid;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<GridScript>();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        collider = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        //       PlayerMovement();
+        PlaceBomb();
+        TurnOnCollider();
+
+    }
+
+    private void PlaceBomb()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!(maxBombs == bombsOnField) && Time.time>nextBombTime)
+            {
+                bombList.Add(Instantiate(bomb, grid.GetNearestPointOnGrid(transform.position) , Quaternion.identity));
+                nextBombTime = Time.time + cooldownTime;
+                bombsOnField++;
+            }
+        }    
+    }
+
+    public void BombExploded()
+    {
+        Debug.Log("BOOM");
+        bombList.RemoveAt(0);
+        bombsOnField--;
+        
+    }
+
+    public void IncreaseMaxBombs()
+    {
+        this.maxBombs++;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            other.gameObject.SetActive(false);
+            this.IncreaseMaxBombs();
+        }
+    }
+
+    private void TurnOnCollider()
+    {
+        if (bombList.Count != 0)
+        {
+            if (Vector3.Distance(bombList[bombsOnField - 1].transform.position, transform.position) > 0.9)
+            {
+                bombList[bombsOnField - 1].GetComponent<Collider>().enabled = true;
+            }
+        }
+    }
+
+}
